241ecdeedfde4e814197830b9305c8d4dbe0a577
Gui Farias <gui.farias@outlook.com.br>
2026-02-13 02:29:34 -0300

    change name fix front2

--- src/components/chat/ChatList.tsx
+++ src/components/chat/ChatList.tsx
@@ -19,7 +19,6 @@ const ChatList = ({ chats, selectedChatId, onSelectChat }: ChatListProps) => {
   const sortedChats = [...chats].sort((a, b) => {
     const aTime = a.lastMessage?.timestamp || a.updatedAt;
     const bTime = b.lastMessage?.timestamp || b.updatedAt;
-    console.log(sortedChats)
     return new Date(bTime).getTime() - new Date(aTime).getTime();
   });
 
